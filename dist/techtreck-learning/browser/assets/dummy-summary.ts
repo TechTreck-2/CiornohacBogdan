7654c9332628e7539293d96fbc2e59fed276b3c3
BogdannCiornohac <bciornoh@emeal.nttdata.com>
2025-07-24 20:25:18 +0300

    dist update

---
+++ dist/techtreck-learning/browser/assets/dummy-summary.ts
@@ -0,0 +1,190 @@
+import { getDaysBetween } from '../app/core/functions/daysBetween';
+import { rowDatasummary, ticketsType } from '../app/types/types';
+
+export const summaryData: rowDatasummary[] = [
+  {
+    date: '2024-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-01-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2022-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2024-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+  {
+    date: '2023-09-01',
+    type: 'Pontat',
+    start: '08:30',
+    stop: '17:30',
+    duration: '09:00',
+  },
+];
+
+/*
+  
+export const vacationTickets: ticketsType[] = [
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2025-03-02',
+    finish: '2025-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2025-03-02', '2025-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-09-21',
+    finish: '2024-10-03',
+    status: 'In asteptare',
+    duration: getDaysBetween('2024-09-21', '2024-10-03'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Vacanta',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+];
+
+export const permissionsTickets: ticketsType[] = [
+  {
+    type: 'Bilet de voie',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+  {
+    type: 'Bilet de voie',
+    start: '2025-03-02',
+    finish: '2025-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2025-03-02', '2025-03-12'),
+  },
+  {
+    type: 'Bilet de voie',
+    start: '2024-09-21',
+    finish: '2024-10-03',
+    status: 'In asteptare',
+    duration: getDaysBetween('2024-09-21', '2024-10-03'),
+  },
+  {
+    type: 'Bilet de voie',
+    start: '2024-03-02',
+    finish: '2024-03-12',
+    status: 'Aprobat',
+    duration: getDaysBetween('2024-03-02', '2024-03-12'),
+  },
+];
+
+*/
---
+++ dist/techtreck-learning/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SWCBEZ2M.mjs";import"./chunk-MAET2JQG.mjs";import"./chunk-4YG7KSJ4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
